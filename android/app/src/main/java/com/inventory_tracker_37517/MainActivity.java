2d4504bdb74ac72c95b5d8ad73f5db3bbe19173a
Crowdbotics <team@crowdbotics.com>
2022-11-08 01:00:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/inventory_tracker_37517/MainActivity.java
@@ -0,0 +1,15 @@
+package com.inventory_tracker_37517;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "inventory_tracker_37517";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "inventory_tracker_37517",
+  url: "https://inventory_tracker_37517.botics.co",
+  api: "https://inventory_tracker_37517.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
